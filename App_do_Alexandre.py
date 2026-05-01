8d3b5fc19308a49c9d5298b1fef471b26bf8b6c6
abrancoteams10-creator <abranco.teams10@gmail.com>
2026-05-01 19:07:40 -0300

    Update App_do_Alexandre.py

--- App_do_Alexandre.py
+++ App_do_Alexandre.py
@@ -3,4 +3,5 @@ import streamlit as st
 
 st.title('Qual é o último título de cada clube?')
 
-st.write('Esse aplicativo, busca informar e divertir os usúarios, dando informações sobre os times e seus ultimos títulos')
+st.write('Esse aplicativo, busca informar e divertir os usúarios, dando informações sobre os times e seus ultimos títulos.')
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTvp2MMXNSlXh-menVUzXw1IX8rnmmCP_-NQ&s')
