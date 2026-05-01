5d9ff51324ae2068c51556160b275b0ecfcb748c
abrancoteams10-creator <abranco.teams10@gmail.com>
2026-05-01 19:17:56 -0300

    Update App_do_Alexandre.py

--- App_do_Alexandre.py
+++ App_do_Alexandre.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title('Qual foi o último título de cada clube?')
 
-st.write('Este aplicativo, busca informar e divertir os usúarios, dando informações sobre os times e seus últimos títulos conquistados.')
+st.write('Este aplicativo tem por objetivo informar e divertir os usúarios, dando informações sobre os times e seus últimos títulos conquistados.')
 st.image('https://us.123rf.com/450wm/laymul/laymul2001/laymul200100409/137643792-bola-de-futebol-e-trof%C3%A9u.jpg?ver=6')
 
 st.write('')
