207c8282427dda5c81e05891a860eb594d7ace32
abrancoteams10-creator <abranco.teams10@gmail.com>
2026-05-01 21:14:18 -0300

    Update Manual_dos_amantes_de_futebol.py

--- Manual_dos_amantes_de_futebol.py
+++ Manual_dos_amantes_de_futebol.py
@@ -74,7 +74,7 @@ dados_times = {
         "publico": "41.986 presentes",
         "campeonato": "Campeonato Brasileiro 2024",
         "cor": "#005baa",
-       if st.session_state.time:
+if st.session_state.time:
     info = dados_times[st.session_state.time]
     
     # Esta linha executa o áudio baseado no que está no dicionário do time
