1c4db39f211a7563e0f68680c4d9267d8788f1d3
abrancoteams10-creator <abranco.teams10@gmail.com>
2026-05-01 21:12:35 -0300

    Update Manual_dos_amantes_de_futebol.py

--- Manual_dos_amantes_de_futebol.py
+++ Manual_dos_amantes_de_futebol.py
@@ -74,7 +74,11 @@ dados_times = {
         "publico": "41.986 presentes",
         "campeonato": "Campeonato Brasileiro 2024",
         "cor": "#005baa",
-       
+       if st.session_state.time:
+    info = dados_times[st.session_state.time]
+    
+    # Esta linha executa o áudio baseado no que está no dicionário do time
+    st.audio(info["musica"], format="audio/mp3")
         "img_url":"https://lncimg.lance.com.br/uploads/2024/12/botafogo-campeao-brasileiro-scaled-aspect-ratio-512-320.jpg"
     },
 }
